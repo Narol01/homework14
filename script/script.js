5ac98d0fe5cf96b9cee545b6222c68e691c58d2a
narol01 <kmingogod@gmail.com>
2024-01-28 19:53:51 +0100

    first commit

--- script/script.js
+++ script/script.js
@@ -22,10 +22,24 @@ event.target.reset();
 function getName(name, lastName,age){
     const container=document.createElement("div");
     const dataNode=document.createElement("p");
+    const ageNode=document.createElement("p");
 
-    dataNode.innerText=name+" "+ lastName+ " " + age;
 
-    container.append(dataNode);   
+    dataNode.innerText=name+" "+ lastName;
+    ageNode.innerText="Возраст: "+ age;
+    container.classList.add("item")
+    if(age<=18){
+        container.style.background="#1abc9c";
+     }
+    if(age<=35 & age>=19){
+    container.style.background="#f39c12";
+     }
+    if(age>=35){
+    container.style.background="#d35400";
+     }
+    
+
+    container.append(dataNode , ageNode);   
     return container;
 }
 
